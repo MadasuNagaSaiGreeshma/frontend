4857ea96aa3906b046c42d8acd1cb7ce7ee42d00
MadasuNagaSaiGreeshma <greeshmamadasu@gmail.com>
2025-06-19 11:19:07 +0530

    Add files via upload

---
+++ methods/Methods.java
@@ -0,0 +1,55 @@
+/* class Methods
+{
+public void add()
+{
+System.out.println(10+20);
+}
+static public void main(String args[])
+{
+Methods m=new Methods();
+m.add();
+}
+}  */
+
+/* class Methods
+{
+public void sub(int a,int b)
+{
+System.out.println(a-b);
+}
+static public void main(String args[])
+{
+Methods m=new Methods();
+m.sub(20,10);
+}
+}   */	
+
+/* class Methods
+{
+public void mul()
+{
+System.out.println(10*20);
+}
+static public void main(String args[])
+{
+Methods m=new Methods();
+m.mul();
+return ;
+}
+}    */
+
+/* class Methods
+{
+public void div(int a,int b)
+{
+System.out.println(a/b);
+}
+static public void main(String args[])
+{
+Methods m=new Methods();
+m.div(20,10);
+return;
+}
+}   */
+
+
