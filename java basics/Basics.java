64e392d96e6f05ce0219aa25c9e114a989042da0
MadasuNagaSaiGreeshma <greeshmamadasu@gmail.com>
2025-06-20 12:27:35 +0530

    Add files via upload

---
+++ java basics/Basics.java
@@ -0,0 +1,106 @@
+/* 1 class Basics
+{
+public void name()
+{
+System.out.println("Sri Vasavi");
+}
+public static void main(String[] args)
+{
+Basics b=new Basics();
+b.name
+();
+}
+}
+output:
+D:\29>java Basics
+Sri Vasavi             */
+
+
+/* 2 class Basics
+{
+public static void main(String[] args)
+{
+System.out.println("M.Greeshma");
+}
+}
+output:
+M.Greeshma        */
+
+/* 3 class Basics
+{ 
+/**void s()
+{
+System.out.println("M.Greeshma");
+}*/
+/* public static void main(String[] args)
+{  
+/* Basics b=new Basics();
+b.s();*/
+//System.out.println("Pawan kalyam");//
+/* System.out.println("ECE");
+}
+}              */
+
+
+/* 4 class Basics
+{
+public static void main(String[] args)
+{
+int a=29;
+boolean b=true;
+char c='D';
+float d= 71.9f;   
+double e= 99.99;
+System.out.println("Integer value: "+a);
+System.out.println("Boolean value: "+b);
+System.out.println("Character value: "+c);
+System.out.println("Float value: " +d);
+System.out.println("Double value: " +e);
+}
+}      
+output:
+Integer value: 29
+Boolean value: true
+Character value: A
+Float value: 71.9
+Double value: 99.99                 */
+
+/*  5 class Basics 
+{
+int x = 10;
+public void local() {
+int x =30;
+System.out.println("The Local variable is : " + x);   
+}
+public static void main(String[] args) 
+{
+Basics b = new Basics();
+b.local();
+System.out.println("The Global Variable is : "+b.x);
+}
+}
+output:
+The Local variable is : 30
+The Global Variable is : 10                 */
+
+
+
+/* 6 class Basics
+{
+public void name()
+{
+System.out.println("M.N.S.Greshma");
+}
+public static void main(String[] args) 
+{
+Basics b=new Basics();
+b.name();
+}
+}
+output:
+M.N.S.Greshma                                     */
+
+
+
+
+
